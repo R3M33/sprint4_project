94f60a17b30eceff93b382b79b59e6f9dc04989e
R3M33 <tonyrcamp@hotmail.com>
2024-05-02 16:44:49 -0400

    This should be the final alteration of the repository. All conditons and suggestions have bwen met.

--- app.py
+++ app.py
@@ -14,37 +14,26 @@ st.write('The file contains no duplicates but does have alot of missing values i
 
 st.write('Conclusion: Based on the charts usually, the more recent a car was made the more you pay for it.')
 
-# Load data
 cars = pd.read_csv('vehicles_us (1).csv')
 
-# Create a checkbox to filter data
 new_models = st.checkbox("Show only cars from 2013 or older")
 
 if new_models:
     cars = cars[cars['model_year'] <= 2013]
 
-# Create a checkbox to filter expensive cars
 expensivecars = st.checkbox("Show only cars over $50k")
 
 if expensivecars:
     cars = cars[cars['price'] > 50000]
 
-# Create two columns for displaying the graphs
-col1, col2 = st.columns(2)
+st.subheader('Histogram')
+fig = px.histogram(cars, x='model_year', y='price')
+fig.update_layout(xaxis_title='Model Year', yaxis_title='Price')
+fig.update_layout(title='Car Sales Histogram')
+st.plotly_chart(fig)
 
-# Display histogram in the first column
-with col1:
-    st.subheader('Histogram')
-    fig = px.histogram(cars, x='model_year', y='price')
-    fig.update_layout(xaxis_title='Model Year', yaxis_title='Price')
-    fig.update_layout(title = 'Car Sales Histogram')
-    st.plotly_chart(fig)
-
-# Display scatter plot in the second column
-with col2:
-    st.subheader('Scatter Plot')
-    fig2 = px.scatter(cars, x='model_year', y='price')
-    fig2.update_layout(xaxis_title='Model Year', yaxis_title='Price')
-    fig2.update_layout(title = 'Car Sales Scatter Plot')
-    st.plotly_chart(fig2)
-
+st.subheader('Scatter Plot')
+fig2 = px.scatter(cars, x='model_year', y='price')
+fig2.update_layout(xaxis_title='Model Year', yaxis_title='Price')
+fig2.update_layout(title='Car Sales Scatter Plot')
+st.plotly_chart(fig2)
